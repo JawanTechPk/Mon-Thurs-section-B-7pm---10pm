f73a836acd55301cc6e7ffe80b201a87f1343035
jawantechpk <jawantechpk@gmail.com>
2021-11-10 20:10:33 +0500

    update
    update

---
+++ class 24 04-11-2021/app.js
@@ -0,0 +1,118 @@
+// var scoreAvg = 2.434523;
+
+// scoreAvg = Math.round(scoreAvg);
+
+// console.log(scoreAvg);
+
+
+
+// var scoreAvg = 1.1;
+
+// scoreAvg = Math.ceil(scoreAvg);
+
+// console.log(scoreAvg)
+
+
+
+// var scoreAvg = -1.1;
+
+// scoreAvg = Math.ceil(scoreAvg);
+
+// console.log(scoreAvg)
+
+
+// console.log(Math.random())
+
+
+// var bigDecimal = Math.random();
+//  var improvedNum = (bigDecimal * 50) + 1;
+//  var numberOfStars = Math.floor(improvedNum);
+
+//  console.log(numberOfStars)
+
+// var username = prompt("Enter Username");
+
+// for(var i = 0; i < username.length; i++){
+//     if(username.slice(i, i+1).charCodeAt() == "33" || username.slice(i, i+1).charCodeAt() == "33"){}
+// }
+
+// var password = prompt("Enter Password");
+
+// var startingCond = false;
+// var lengthCond = false;
+// var alphabetCond = false;
+// var numberCond = false;
+
+
+// if(password.slice(0,1).charCodeAt() >= 48 && password.slice(0,1).charCodeAt() <= 57){
+//     startingCond = true;
+// }
+
+// if(password.length < 6){
+//     lengthCond = true;
+// }
+
+
+// for(var i = 0; i < password.length; i++){
+//     if((password.slice(i, i+1).charCodeAt() >= "65" && password.slice(i, i+1).charCodeAt() <= "90") || (password.slice(i, i+1).charCodeAt() >= "97" && password.slice(i, i+1).charCodeAt() <= "122")){
+//         alphabetCond = true;
+//     }
+// }
+
+// for(var j = 1; j < password.length; j++){
+//     if (password.slice(j , j+1).charCodeAt() >= "48" && password.slice(j , j+1).charCodeAt() >= "57") {\
+//         numberCond = true;
+        
+//     }
+// }
+
+// if(startingCond)
+
+
+
+
+
+
+// console.log( "100" - "50" );
+// console.log( "100" - 50 );
+
+
+// console.log( "100" * "50" );
+// console.log( "100" * 50 );
+
+
+// console.log( "100" / "50" );
+// console.log( "100" / 50 );
+
+
+// console.log( "100" / "khan" );
+
+
+// console.log( parseInt("100") + "50" );
+// console.log(  parseInt("100") + 50 );
+
+
+
+// console.log( parseInt("100.08755") + "50" );
+// console.log(  parseInt("100.08755") + 50 );
+
+
+// console.log( parseFloat("100.08755") + "50" );
+// console.log(  parseFloat("100.08755") + 50 );
+
+
+// console.log( Number("100.08755") + "50" );
+// console.log(  Number("100.08755") + 50 );
+
+
+
+// var num = 13123213
+// num = num.toString();
+
+// console.log(typeof num)
+
+
+
+
+var num = 1.324797324;
+console.log(num.toFixed(5))
---
+++ class 25 8-11-2021/app.js
@@ -0,0 +1,72 @@
+
+// var rightNow = new Date();
+// console.log(rightNow)
+
+// var str = rightNow.toString()
+// console.log(str)
+
+// var rightNow = new Date();
+// var day = rightNow.getDay()
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// console.log(day)
+
+
+// var rightNow = new Date();
+// var month = rightNow.getMonth()
+// console.log(month)
+
+// var rightNow = new Date();
+// var date = rightNow.getDate()
+// console.log(date)
+
+
+// var rightNow = new Date();
+// var hour = rightNow.getHours()
+// console.log(hour)
+
+
+// var rightNow = new Date();
+// var miliSec = rightNow.getTime()
+
+// var numDays = miliSec/(1000*60*60*24);
+// var numDays =  Math.round(numDays)
+// console.log(numDays)
+
+
+// var rightNow = new Date();
+// var hour = rightNow.getHours()
+// var min = rightNow.getMinutes()
+// var sec = rightNow.getSeconds()
+
+// console.log(hour + " : " + min + " : " + sec)
+
+
+// var today = new Date();
+// var futureDay = new Date("january 25, 2045");
+
+
+// var msToday = today.getTime()
+// var msFutureDay = futureDay.getTime()
+
+// var remainingTime = msFutureDay - msToday;
+
+// var remainingTime = Math.round(remainingTime / (1000 *60*60*24))
+// console.log(remainingTime)
+
+// var today = new Date();
+
+// console.log(today);
+
+// today.setFullYear(2000)
+
+// today.setMonth(5)
+
+// console.log(today)
+
+
+
+
+
+
+ 
