cee7bd7214e42e485a2e653675953824166f164f
jawantechpk <jawantechpk@gmail.com>
2021-11-19 22:05:54 +0500

    55 chapter
    55 chapter

---
+++ class 31 19-11-2021/app.js
@@ -0,0 +1,105 @@
+
+// alert('Hello World');
+// var useremail = document.getElementById('email').value;
+
+
+// function checkEmail(val1){
+//     // alert('Hello'+val1);
+//     alert(useremail+val1);
+// }
+
+
+
+
+// alert('Hello World');
+// var useremail = document.getElementById('email');
+
+
+// function checkEmail(val1){
+//     // alert('Hello'+val1);
+//     alert(useremail.value+val1);
+// }
+
+
+
+// function expandLoris() {
+//      var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time. Slow lorises have a toxic bite, a rare trait among mammals.";
+//      document.getElementById("slowLoris").innerHTML = expandedParagraph;
+//      }
+
+// var userName = "Umair";
+// userName = 'khan'
+
+
+// function updateDivCont(){
+//     var listVar = "<ol><li>Apple</li><li>Pine Apple</li><li>Mango</li></ol>"
+// document.getElementById('lorisList').innerHTML = listVar
+// }
+
+
+// function getDivCont(){
+//   var val = document.getElementById('lorisList').innerHTML;
+//   console.log(val);
+// }
+
+
+
+// function blobDissapear(){
+//     document.getElementById('ugly').className = 'blobFishClass'
+// }
+
+
+
+// function checkBlobEle(){
+//     var val = document.getElementById('ugly').className;
+//     console.log(val);
+// }
+
+
+
+
+
+// function blobDissapear(){
+//     document.getElementById('ugly').className = 'hidden'
+// }
+
+
+
+// function checkBlobEle(){
+//     var val = document.getElementById('ugly').className;
+//     console.log(val);
+// }
+
+
+// function updateHead(){
+//     document.getElementById('mainHead').className += " head";
+// }
+
+// function checkHeading(){
+//     var val = document.getElementById('mainHead').className;
+//     console.log(val);
+// }
+
+
+
+// function changeImg(){
+//     document.getElementById('ugly').src = "./image/shark.jpg";
+// }
+
+
+
+
+// function changeImg(val1,val2){
+//     document.getElementById(val2).src = val1;
+// }
+
+
+
+// var url = './image/shark.jpg'
+
+// function pass(){
+// return url
+// }
+
+
+var m = document.getElementById("mainPic").style.margin;
